df3c79985094ccbe5661b9a80169401a779b5196
Nick Lopez <nicklopez5454@yahoo.com>
2022-05-14 16:27:54 -0500

    initialized clockedicon componenets

---
+++ client/src/components/ClockedIcon.jsx
@@ -0,0 +1,26 @@
+import { ExpandMore } from "@mui/icons-material"
+import { Accordion, AccordionDetails, AccordionSummary, Avatar, Button, IconButton, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+
+const ClockedIcon = () => {
+return (
+  <Box sx={{position: "fixed",  bottom: "0", right: "0", width: "20%"}}>
+    <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMore />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Chat</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Button variant="square" startIcon={<Avatar/>} sx={{width: "100%", display: "flex", justifyContent:"space-between"}}>
+          
+          UserName
+          </Button>
+        </AccordionDetails>
+      </Accordion>
+  </Box>
+)
+}
+export default ClockedIcon
